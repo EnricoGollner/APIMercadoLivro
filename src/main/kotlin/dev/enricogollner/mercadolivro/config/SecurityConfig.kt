19a4d35fbba8c19d0074c2edd974f6b54a4d2a25
Enrico Gollner <enricogollner.tech@gmail.com>
2024-04-03 23:59:27 -0300

    Updating

--- src/main/kotlin/dev/enricogollner/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/dev/enricogollner/mercadolivro/config/SecurityConfig.kt
@@ -2,11 +2,26 @@ package dev.enricogollner.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig() {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.cors().and().csrf().disable()
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        return http.build()
+    }
+
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
